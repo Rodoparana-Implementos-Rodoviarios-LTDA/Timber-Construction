71d353dab7bbe23a2d8c3a7b390089e40902379b
Guilherme Rodniski Correia <146742503+GuilhermeRCorreia@users.noreply.github.com>
2024-07-08 15:47:23 -0300

    v3

--- src/app/_document.js
+++ src/app/_document.js
@@ -5,19 +5,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-KY94CH7M7R"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-KY94CH7M7R');
-              `,
-            }}
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
