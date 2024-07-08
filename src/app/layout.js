a7d2199753e98cc07a0c1ec13d4846d75598012e
Guilherme Rodniski Correia <146742503+GuilhermeRCorreia@users.noreply.github.com>
2024-07-08 16:04:26 -0300

    v5

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import TransitionProvider from "@/components/transitionProvider";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "TIMBER - SANY",
+  description: "Excelência na Construção, Inovação em Maquinário.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <TransitionProvider>
+          <GoogleAnalytics gaId="G-KY94CH7M7R" />
+          {children}
+        </TransitionProvider>
+      </body>
+    </html>
+  );
+}
